8a55b3666d5fde25dcb29ce3159e788093a51c82
lostnihilist <lostnihilist@01955d66-0541-0410-91e3-e96b89371946>
2008-02-05 16:30:33 +0000

    added test cases for issues 15 and 16

---
+++ branches/breakHelpLess.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""A test case breaking the less-like behavior of help(python_object)
+replacing sys.stdout with the utfWriter seems to be causing the issue
+commenting it out should fix the problem.
+These are just extra lines
+to trigger the less-like behavior of help()
+we have to fill the 
+terminal otherwise less
+will not get activated 
+and the test case is difficult to observe
+
+
+
+
+
+
+
+
+
+
+the end.
+"""
+
+import codecs
+import sys
+utfWriter = codecs.getwriter( "utf-8" )
+#sys.stdout = utfWriter( sys.stdout, "replace" )
+
+
+
---
+++ branches/unicodeDocError.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+u"""import unicodeDocError
+help(unicodeDocError)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in ?
+  File "/usr/lib/python2.4/site.py", line 333, in __call__
+    return pydoc.help(*args, **kwds)
+  File "/usr/lib/python2.4/pydoc.py", line 1656, in __call__
+    self.help(request)
+  File "/usr/lib/python2.4/pydoc.py", line 1700, in help
+    else: doc(request, 'Help on %s:')
+  File "/usr/lib/python2.4/pydoc.py", line 1483, in doc
+    pager(title % desc + '\n\n' + text.document(object, name))
+  File "/usr/lib/python2.4/pydoc.py", line 1311, in pager
+    pager(text)
+  File "/usr/lib/python2.4/pydoc.py", line 1331, in <lambda>
+    return lambda text: pipepager(text, 'less')
+  File "/usr/lib/python2.4/pydoc.py", line 1352, in pipepager
+    pipe.write(text)
+UnicodeEncodeError: 'ascii' codec can't encode character u'deleted to make testcase work' in position 1225: ordinal not in range(128)
+remove the below line to get help(thismodule) to work
+\u2208'
+"""
